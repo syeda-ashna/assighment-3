55d52f77fa431aabbd5fdfc03a01498513d999b2
syeda-ashna <syedabattool@gmail.com>
2024-08-22 05:53:20 -0700

    Add files via upload

---
+++ python.py
@@ -0,0 +1,56 @@
+# area of rectangle
+length= 20
+width= 10
+area =length*width
+print(area)
+
+# tem from F to C
+celsius= 0
+f=celsius*(9/5)+32
+print(f)
+
+# area of a circle
+r= 20
+area=3.14*r*r
+print(area)
+
+# percentage
+a=5
+b=10
+c=5
+d=10
+total=100
+obtained=a+b+c+d
+per=obtained/total
+print(per)
+
+# volume of cylinder
+r=10
+h=5
+v=3.14*r*r*h
+print(v)
+
+# area of cube
+a=5
+A=6*a*a
+print(A)
+
+# age based on current year and birth year
+c=2024
+b= 2006
+age=c-b
+print(age)
+
+# given number of second into minute
+m=15
+s=20
+min=s/60
+sec=m*60
+print(min)
+print(sec)
+
+# BMI
+weight=64
+height=10
+bmi=weight/height*height
+print(bmi)
